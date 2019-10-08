3247d64f40299744a1bef1a87726c1c20f683557
joelpdurham <joeldur@gmail.com>
2019-10-08 11:36:21 -0700

    fixed test

--- test/render-item-test.js
+++ test/render-item-test.js
@@ -7,7 +7,7 @@ test('renders an item', function(assert) {
 const dekuStick = itemArray[0];
     console.log(dekuStick);
 
-    const expected = '<li class="inventory-item" title="Can be used as a torch or weapon."><h3>Deku Stick</h3><img src="./assets/deku-stick.png" alt="Deku Stick image"><p class="price">10<button value="deku-stick">Add</button></p></li>';
+    const expected = '<li class="inventory-item" title="Can be used as a torch or weapon."><h3>Deku Stick</h3><img src="./assets/deku-stick.png" alt="Deku Stick image"><p class="price"><img src="./assets/rupee.png" alt="rupee"> = 10<button value="deku-stick">Add</button></p></li>';
 
     
     const dom = renderItem(dekuStick);
