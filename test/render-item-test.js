c6f170d6bf3204d2a4412d750b5f0f68280d6bd2
joelpdurham <joeldur@gmail.com>
2019-10-08 11:40:01 -0700

    fixed tests, cleaned up console logs

--- test/render-item-test.js
+++ test/render-item-test.js
@@ -4,16 +4,12 @@ import { renderItem } from '../render-item.js';
 const test = QUnit.test;
 
 test('renders an item', function(assert) {
-const dekuStick = itemArray[0];
-    console.log(dekuStick);
+    const dekuStick = itemArray[0];
 
     const expected = '<li class="inventory-item" title="Can be used as a torch or weapon."><h3>Deku Stick</h3><img src="./assets/deku-stick.png" alt="Deku Stick image"><p class="price"><img src="./assets/rupee.png" alt="rupee"> = 10<button value="deku-stick">Add</button></p></li>';
 
-    
     const dom = renderItem(dekuStick);
-    console.log(dom);
     const html = dom.outerHTML;
-    console.log(html);
    
 
     assert.equal(html, expected);
