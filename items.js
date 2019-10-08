c1ff059ef4b0694b471fba4399f23a9f9a5ab536
joelpdurham <joeldur@gmail.com>
2019-10-07 17:46:07 -0700

    created product data, product array, and exported

---
+++ items.js
@@ -0,0 +1,134 @@
+const dekuStick = {
+    id: 'deku-stick',
+    name: 'Deku Stick',
+    image: '',
+    description: 'Can be used as a torch or weapon.',
+    category: 'inventory-item',
+    price: 10
+};
+
+const bomb = {
+    id: 'bomb',
+    name: 'Bomb',
+    image: '',
+    description: 'Used to damage enemies and destroy certain surfaces. Must have a Bomb-Bag',
+    category: 'inventory-item',
+    price: 25
+};
+
+const bombchu = {
+    id: 'bumbchu',
+    name: 'Bombchu',
+    image:'',
+    description: 'A mobile bomb that can travel along the ground, walls, and ceilings',
+    category: 'inventory-item',
+    price: 40
+};
+
+const magicBeans = {
+    id: 'magic-beans',
+    name: 'Magic Beans',
+    image: '',
+    description: 'Plant in soft soil, and wait 7 years.',
+    category: 'inventory-item',
+    price: 10
+};
+
+const keatonMask = {
+    id: 'keaton-mask',
+    name: 'Keaton Mask',
+    image: '',
+    description: 'The Keaton character is renowned throughout Hyrule as a popular figure, predominately among children.',
+    category: 'mask',
+    price: 10
+};
+
+const skullMask = {
+    id: 'skull-mask',
+    name: 'Skull Mask',
+    image: '',
+    description: 'A strange mask. Prevents Keese attacks.',
+    category: 'mask',
+    price: 20
+};
+
+const spookyMask = {
+    id: 'spooky-mask',
+    name: 'Spooky Mask',
+    image: '',
+    description: 'Carved from wood taken from a coffin. Glows in the dark. Scares most people.',
+    category: 'mask',
+    price: 30
+};
+
+const bunnyHood = {
+    id: 'bunny-hood',
+    name: ',Bunny Hood',
+    image: '',
+    description: 'Prevents Stalchildren from appearing at night.',
+    category: 'mask',
+    price: 40
+};
+
+const redPotion = {
+    id: 'red-potion',
+    name: 'Red Potion',
+    image: '',
+    description: 'Fills up to 8 heart pieces.',
+    category: 'bottled-item',
+    price: 40
+};
+
+const greenPotion = {
+    id: 'green-potion',
+    name: 'Green Potion',
+    image: '',
+    description: 'Fills up your magic meter.',
+    category: 'bottled-item',
+    price: 50
+};
+
+const bluePotion = {
+    id: 'blue-potion',
+    name: 'Blue Potion',
+    image: '',
+    description: 'Fills both your hearts and magic meter.',
+    category: 'bottled-item',
+    price: 60
+};
+
+const fairy = {
+    id: 'fairy',
+    name: 'Fairy',
+    image: '',
+    description: 'If you die, will bring you back to life with 8 hearts.',
+    category: 'bottled-item',
+    price: 40
+};
+
+const lonLonMilk = {
+    id: 'lon-lon-milk',
+    name: 'Lon Lon Milk',
+    image: '',
+    description: 'Two servings. Each serving refills 6 hearts.',
+    category: 'bottled-item',
+    price: 40
+};
+
+const itemArray = [
+    dekuStick, 
+    bomb, 
+    bombchu, 
+    magicBeans, 
+    keatonMask, 
+    skullMask, 
+    spookyMask, 
+    bunnyHood, 
+    redPotion, 
+    greenPotion, 
+    bluePotion, 
+    fairy, 
+    lonLonMilk
+];
+
+export default itemArray;
