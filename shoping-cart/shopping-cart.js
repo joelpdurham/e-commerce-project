3f3bcf13d1473ac2b2c2cb22f7672c3fb4863c98
joelpdurham <joeldur@gmail.com>
2019-10-09 10:40:38 -0700

    cart items render on table. feelin like a million rupees

--- shoping-cart/shopping-cart.js
+++ shoping-cart/shopping-cart.js
@@ -1,3 +1,9 @@
+import { itemArray } from '../api.js';
+import { makeTableRow } from './render-line-item.js';
+import { findById } from '../utils.js';
+
+const cartContents = document.getElementById('cart-import');
+
 export const cart = [{
     item_id: 'deku-stick',
     quantity: 5
@@ -11,3 +17,12 @@ export const cart = [{
     item_id: 'bomb',
     quantity: 4
 }];
+
+for (let i = 0; i < cart.length; i++) {
+    const lineRow = cart[i];
+    const item = findById(itemArray, lineRow.item_id);
+    const dom = makeTableRow(item, lineRow);
+
+    cartContents.appendChild(dom);
+}
+
