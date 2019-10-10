cf8d57c562b51c4fa00e3b70e45a075141e113ad
joelpdurham <joeldur@gmail.com>
2019-10-10 11:56:24 -0700

    place order button diabled works when cart is empty.

--- shoping-cart/shopping-cart.js
+++ shoping-cart/shopping-cart.js
@@ -4,19 +4,28 @@ import { findById, calcLineItem } from '../utils.js';
 
 const cartContents = document.getElementById('cart-import');
 const cartTotalContents = document.getElementById('order-total-cell');
+const placeOrder = document.getElementById('place-order');
 let cartTotal = 0;
 
 let json = localStorage.getItem('CART');
 let cart = JSON.parse(json);
+console.log(cart);
 
-for (let i = 0; i < cart.length; i++) {
-    const lineRow = cart[i];
-    const quantity = cart[i].quantity;
-    const item = findById(itemArray, lineRow.id);
-    const dom = makeTableRow(item, lineRow);
-    cartTotal = cartTotal + calcLineItem(quantity, item.price);
-    
-    cartContents.appendChild(dom);
-    cartTotalContents.textContent = cartTotal;
+
+if (cart === null) {
+    placeOrder.disabled = true;
+    console.log('button off');
+} else if (cart.length > 0) {
+    placeOrder.disabled = false;
+    console.log('button on');
+    for (let i = 0; i < cart.length; i++) {
+        const lineRow = cart[i];
+        const quantity = cart[i].quantity;
+        const item = findById(itemArray, lineRow.id);
+        const dom = makeTableRow(item, lineRow);
+        cartTotal = cartTotal + calcLineItem(quantity, item.price);
+                    
+        cartContents.appendChild(dom);
+        cartTotalContents.textContent = cartTotal;
+    }
 }
-
